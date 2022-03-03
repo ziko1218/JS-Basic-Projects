64877813a21c8231b1b24f132d1afdfb97d788fc
Tsvetoslav Velkov <49896796+ziko1218@users.noreply.github.com>
2022-03-03 10:32:48 +0200

    Add files via upload

---
+++ Преговор FOR.js
@@ -0,0 +1,28 @@
+/* for (let i = 1; i<=3 ;){
+console.log(i); 
+} */
+
+/*
+for (; ;) {
+    console.log('Softuni');
+} */
+
+/*
+for (let i = 0; i < 2 ; i += 0.5) {
+    console.log (i + ", ");
+}
+*/
+
+/*
+let index = 0;
+while (true) {
+    let str = input [index];
+    index++;
+    if (str === 'Stop') {
+        break;
+    }
+     console.log('Infinite loop');
+}
+*/
+
+
